7c02be5c39979dc780a9e90f92cbb5a208100be7
vaishnavme <vaishnavchandurkar21@outlook.com>
2020-12-12 17:24:34 +0530

    Feat: index playing

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,32 +1,66 @@
 const controlButton = document.querySelector("#play-stop");
+
 const circleBig = document.querySelector("#circle-bg");
 const circleSm = document.querySelector("#circle-sm");
+
+const songName = document.querySelector("#song-name");
+const audio = document.querySelector("#audio");
+
 // control button images
 let playImg = "./assets/images/play.svg";
 let pauseImg = "./assets/images/pause.svg";
 // default control image
 controlButton.src = playImg;
 
-let musicSrc = "./assets/music/Lofi1.mp3"
-// player event 
-controlButton.addEventListener("click", playHandler);
+const songList = [
+    {
+        name: "Travel Love Beats",
+        source: "./assets/music/Lofi1.mp3",
+        cover: "./assets/images/chillhop.jpg"
+    },
+    {
+        name: "Night Sky Unreated",
+        source: "./assets/music/Lofi2.mp3",
+        cover: "./assets/images/chillhop-2.jpg"
+    }
+];
 
+let index = 0;
+// preloaded song
+loadSong(songList[index]);
+
+function loadSong() {
+    songName.innerText = songList[index].name;
+    audio.src = songList[index].source;
+}
+//default
 let isPlaying = true;
 
 function play() {
     controlButton.src = pauseImg;
     circleBig.classList.add("animate");
     circleSm.classList.add("animate");
+    // play song
+    audio.play();
 }
 
 function pause() {
     controlButton.src = playImg;
     circleBig.classList.remove("animate");
     circleSm.classList.remove("animate");
+
+    // pause song
+    audio.pause();
 }
 
 function playHandler() {
     isPlaying = !isPlaying;
-    console.log(isPlaying)
+    //console.log("Change: ",isPlaying)
     isPlaying ? pause() : play();
 }
+
+
+// player event 
+controlButton.addEventListener("click", playHandler);
+mBox.addEventListener("click", playAnySong);
+coverArtHandler();
